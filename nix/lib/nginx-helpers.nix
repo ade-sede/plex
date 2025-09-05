dc78be33a9f2433909134508f22f36bb5339e9be
Adrien DE SEDE <adrien.de.sede@gmail.com>
2025-09-05 14:07:25 +0200

    refactor

---
+++ nix/lib/nginx-helpers.nix
@@ -0,0 +1,47 @@
+{...}: {
+  standardProxyHeaders = ''
+    proxy_buffering off;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
+    proxy_set_header X-Forwarded-Host $http_host;
+  '';
+
+  plexProxyHeaders = ''
+    proxy_set_header X-Plex-Client-Identifier $http_x_plex_client_identifier;
+    proxy_set_header X-Plex-Device $http_x_plex_device;
+    proxy_set_header X-Plex-Device-Name $http_x_plex_device_name;
+    proxy_set_header X-Plex-Platform $http_x_plex_platform;
+    proxy_set_header X-Plex-Platform-Version $http_x_plex_platform_version;
+    proxy_set_header X-Plex-Product $http_x_plex_product;
+    proxy_set_header X-Plex-Token $http_x_plex_token;
+    proxy_set_header X-Plex-Version $http_x_plex_version;
+    proxy_set_header X-Plex-Nocache $http_x_plex_nocache;
+    proxy_set_header X-Plex-Provides $http_x_plex_provides;
+    proxy_set_header X-Plex-Device-Vendor $http_x_plex_device_vendor;
+    proxy_set_header X-Plex-Model $http_x_plex_model;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
+    proxy_set_header X-Forwarded-Host $http_host;
+  '';
+
+  mkPublicLocation = port: subpath: extraHeaders: {
+    proxyPass = "http://127.0.0.1:${toString port}${subpath}";
+    proxyWebsockets = true;
+    extraConfig = extraHeaders;
+  };
+
+  mkProtectedLocation = path: port: subpath: serviceName: httpAuth: extraHeaders: {
+    proxyPass = "http://127.0.0.1:${toString port}${subpath}";
+    proxyWebsockets = true;
+    basicAuth = "${serviceName} Access";
+    basicAuthFile = "${httpAuth}";
+    extraConfig =
+      extraHeaders
+      + (
+        if path != "/"
+        then "proxy_set_header X-Forwarded-Prefix ${path};\n"
+        else ""
+      );
+  };
+}
--- nix/services/nginx.nix
+++ nix/services/nginx.nix
@@ -1,14 +1,15 @@
 {
   config,
   pkgs,
-  email,
-  qbittorrentWebUIPort,
-  HTTP_PASSWORD,
+  ADMIN_EMAIL,
+  SECRET_HTTP_PASSWORD,
   ...
 }: let
-  # Create htpasswd file for HTTP auth using openssl
+  nginxHelpers = import ../lib/nginx-helpers.nix {};
+  inherit (nginxHelpers) standardProxyHeaders plexProxyHeaders mkPublicLocation mkProtectedLocation;
+
   httpAuth = pkgs.runCommand "http-htpasswd" {} ''
-    echo "ade-sede:$(${pkgs.openssl}/bin/openssl passwd -apr1 '${HTTP_PASSWORD}')" > $out
+    echo "ade-sede:$(${pkgs.openssl}/bin/openssl passwd -apr1 '${SECRET_HTTP_PASSWORD}')" > $out
   '';
 in {
   services.nginx = {
@@ -22,126 +23,22 @@ in {
       "media.ade-sede.com" = {
         enableACME = true;
         forceSSL = true;
-        locations."/plex/" = {
-          proxyPass = "http://127.0.0.1:32400/";
-          proxyWebsockets = true;
-          extraConfig = ''
-            proxy_set_header X-Plex-Client-Identifier $http_x_plex_client_identifier;
-            proxy_set_header X-Plex-Device $http_x_plex_device;
-            proxy_set_header X-Plex-Device-Name $http_x_plex_device_name;
-            proxy_set_header X-Plex-Platform $http_x_plex_platform;
-            proxy_set_header X-Plex-Platform-Version $http_x_plex_platform_version;
-            proxy_set_header X-Plex-Product $http_x_plex_product;
-            proxy_set_header X-Plex-Token $http_x_plex_token;
-            proxy_set_header X-Plex-Version $http_x_plex_version;
-            proxy_set_header X-Plex-Nocache $http_x_plex_nocache;
-            proxy_set_header X-Plex-Provides $http_x_plex_provides;
-            proxy_set_header X-Plex-Device-Vendor $http_x_plex_device_vendor;
-            proxy_set_header X-Plex-Model $http_x_plex_model;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Host $http_host;
-          '';
-        };
-        locations."/jellyfin/" = {
-          proxyPass = "http://127.0.0.1:8096/";
-          proxyWebsockets = true;
-          extraConfig = ''
-            proxy_buffering off;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Host $http_host;
-          '';
-        };
-        locations."/torrent/" = {
-          proxyPass = "http://127.0.0.1:${toString qbittorrentWebUIPort}/";
-          proxyWebsockets = true;
-          basicAuth = "qBittorrent Access";
-          basicAuthFile = "${httpAuth}";
-          extraConfig = ''
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Host $http_host;
-            proxy_set_header X-Forwarded-Prefix /torrent;
-          '';
-        };
-        locations."/sonarr/" = {
-          proxyPass = "http://127.0.0.1:8989/sonarr/";
-          proxyWebsockets = true;
-          basicAuth = "Sonarr Access";
-          basicAuthFile = "${httpAuth}";
-          extraConfig = ''
-            proxy_buffering off;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Host $http_host;
-            proxy_set_header X-Forwarded-Prefix /sonarr;
-          '';
-        };
-        locations."/prowlarr/" = {
-          proxyPass = "http://127.0.0.1:9696/prowlarr/";
-          proxyWebsockets = true;
-          basicAuth = "Prowlarr Access";
-          basicAuthFile = "${httpAuth}";
-          extraConfig = ''
-            proxy_buffering off;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Host $http_host;
-            proxy_set_header X-Forwarded-Prefix /prowlarr;
-          '';
-        };
-        locations."/radarr/" = {
-          proxyPass = "http://127.0.0.1:7878/radarr/";
-          proxyWebsockets = true;
-          basicAuth = "Radarr Access";
-          basicAuthFile = "${httpAuth}";
-          extraConfig = ''
-            proxy_buffering off;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Host $http_host;
-            proxy_set_header X-Forwarded-Prefix /radarr;
-          '';
-        };
-        locations."/bazarr/" = {
-          proxyPass = "http://127.0.0.1:6767/bazarr/";
-          proxyWebsockets = true;
-          basicAuth = "Bazarr Access";
-          basicAuthFile = "${httpAuth}";
-          extraConfig = ''
-            proxy_buffering off;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Host $http_host;
-            proxy_set_header X-Forwarded-Prefix /bazarr;
-          '';
-        };
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:5055/";
-          proxyWebsockets = true;
-          basicAuth = "Jellyseerr Access";
-          basicAuthFile = "${httpAuth}";
-          extraConfig = ''
-            proxy_buffering off;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Host $http_host;
-          '';
-        };
+
+        locations."/" = mkPublicLocation 5055 "/" standardProxyHeaders; # Jellyseerr
+        locations."/plex/" = mkPublicLocation 32400 "/" plexProxyHeaders;
+        locations."/jellyfin/" = mkPublicLocation 8096 "/" standardProxyHeaders;
+
+        locations."/torrent/" = mkProtectedLocation "/torrent/" 8080 "/" "qBittorrent" httpAuth standardProxyHeaders;
+        locations."/sonarr/" = mkProtectedLocation "/sonarr/" 8989 "/sonarr/" "Sonarr" httpAuth standardProxyHeaders;
+        locations."/prowlarr/" = mkProtectedLocation "/prowlarr/" 9696 "/prowlarr/" "Prowlarr" httpAuth standardProxyHeaders;
+        locations."/radarr/" = mkProtectedLocation "/radarr/" 7878 "/radarr/" "Radarr" httpAuth standardProxyHeaders;
+        locations."/bazarr/" = mkProtectedLocation "/bazarr/" 6767 "/bazarr/" "Bazarr" httpAuth standardProxyHeaders;
       };
     };
   };
 
   security.acme = {
     acceptTerms = true;
-    defaults.email = email;
+    defaults.email = ADMIN_EMAIL;
   };
 }
