8fcdffdd96f281c5ea13b934e1b8a17bebfe23ff
Adrien DE SEDE <adrien.de.sede@gmail.com>
2025-09-05 10:50:04 +0200

    iteration

--- nix/jellyseerr.nix
+++ nix/jellyseerr.nix
@@ -13,9 +13,25 @@
     configDir = "${mountPoint}/jellyseerr";
   };
 
-  systemd.services.jellyseerr = {
-    after = ["network.target" "juicefs-mount.service"];
+  systemd.services.jellyseerr-setup = {
+    before = ["jellyseerr.service"];
+    requiredBy = ["jellyseerr.service"];
+    after = ["juicefs-mount.service"];
     requires = ["juicefs-mount.service"];
+    script = ''
+      mkdir -p "${mountPoint}/jellyseerr"
+      chmod 755 "${mountPoint}/jellyseerr"
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      User = "root";
+    };
+  };
+
+  systemd.services.jellyseerr = {
+    after = ["network.target" "juicefs-mount.service" "jellyseerr-setup.service"];
+    requires = ["juicefs-mount.service" "jellyseerr-setup.service"];
     bindsTo = ["juicefs-mount.service"];
   };
 }
